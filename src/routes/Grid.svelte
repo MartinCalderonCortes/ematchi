3ed25f781bb0129c1158040dabf6943a74867190
Martin <mh.straus@hotmail.com>
2026-05-30 15:40:19 -0500

    Include the logic on the board to know the match between the cells pair selected for the user. Add a dispatch event to sendto the Game component the emoji found

--- src/routes/Grid.svelte
+++ src/routes/Grid.svelte
@@ -1,23 +1,55 @@
 <script lang="ts">
-	import Square from "./Square.svelte";
+	import { createEventDispatcher } from 'svelte';
+	import Square from './Square.svelte';
 
-    export let grid: string[];
+	const dispatch = createEventDispatcher();
 
+	export let grid: string[];
+	export let emojisFound: string[];
+
+	let cellA = -1;
+	let cellB = -1;
+	let resetSelectedCells: number;
 </script>
+
 <div class="grid">
-    {#each grid as emoji}
-        <Square {emoji}/>        
-    {/each}
+	{#each grid as emoji, index}
+		<Square
+			{emoji}
+			selected={cellA === index || cellB === index}
+			on:click={() => {
+				clearInterval(resetSelectedCells);
+				if (cellA === -1 && cellB === -1) {
+					cellA = index;
+				} else if (cellB === -1) {
+					cellB = index;
+					if (grid[cellA] === grid[cellB]) {
+						//match
+						dispatch('found', {
+							emoji
+						});
+					} else {
+						//not mathc
+						resetSelectedCells = setInterval(() => {
+							cellA = cellB = -1;
+						}, 300);
+					}
+				} else {
+					cellA = index;
+					cellB = -1;
+				}
+			}}
+			isEmojiFound={emojisFound.includes(emoji)}
+		/>
+	{/each}
 </div>
 
 <style>
-    .grid {
-        display: grid;
-        grid-template-columns: repeat(4, 1fr);
-        grid-template-rows: repeat(4, 1fr);
-        height: 100%;
-        grid-gap: 4px;
-        background-color: red;
-    }
-
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(4, 1fr);
+		grid-template-rows: repeat(4, 1fr);
+		height: 100%;
+		grid-gap: 2px;
+	}
 </style>
