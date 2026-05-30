d1ac81a412917df771ed6c86cd5895756a673a88
Martin <mh.straus@hotmail.com>
2026-05-30 15:35:44 -0500

    Add EmojisFound component to show on the bottom of the board the emojis matched

---
+++ src/routes/EmojisFound.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	export let emojisFound: string[];
+</script>
+
+<div class="found">
+	{#each emojisFound as emoji}
+		<span>{emoji}</span>
+	{/each}
+</div>
+
+<style>
+	.found {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		gap: 8px;
+		font-size: 6em;
+	}
+
+	span {
+		padding-top: 4px;
+	}
+</style>
