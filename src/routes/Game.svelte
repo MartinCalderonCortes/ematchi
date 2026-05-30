83b9b9266d651694005b9ebdc827cd1fa37b697a
Martin <mh.straus@hotmail.com>
2026-05-30 15:50:05 -0500

    Include shuffle function to generate a random sequences on the list of emojis. Add countdown function with initial logic for the regresive time behavior. Include Contdown and Emojis components on the view

--- src/routes/Game.svelte
+++ src/routes/Game.svelte
@@ -1,58 +1,94 @@
 <script lang="ts">
-	import Grid from "./Grid.svelte";
-	import type { Level } from "./levels";
-	import { levels } from "./levels";
+	import Grid from './Grid.svelte';
+	import type { Level } from './levels';
+	import { levels } from './levels';
+	import { shuffle } from './utils';
+	import EmojisFound from './EmojisFound.svelte';
+	import Countdown from './Countdown.svelte';
+	import { onMount } from 'svelte';
 
-    const level = levels[0];
-    //let size: number = level.size;
-    let grid: string[] = createGrid(level);
-    //let found: string[] = [];
+	const level = levels[0];
+	//let size: number = level.size;
+	let grid: string[] = createGrid(level);
+	let emojisFound: string[] = [];
+	let remaining: number = level.duration;
+	let playing: boolean = false;
 
-    function createGrid(level: Level) {
-        const copy = [...level.emojis]
-        const pairs: string[] = []
+	function createGrid(level: Level) {
+		const copy = [...level.emojis];
+		const pairs: string[] = [];
 
-        for(let i = 0; i < level.size ** 2 / 2; i++){
-            const index = Math.floor(Math.random() * copy.length)
-            const emoji = copy[index]
+		for (let i = 0; i < level.size ** 2 / 2; i++) {
+			const index = Math.floor(Math.random() * copy.length);
+			const emoji = copy[index];
 
-            copy.splice(index, 1)
-            pairs.push(emoji)}
+			copy.splice(index, 1);
+			pairs.push(emoji);
+		}
 
-        pairs.push(...pairs);
+		pairs.push(...pairs);
 
-        return pairs;
-    }
+		return shuffle(pairs);
+	}
 
+	function countdown() {
+		const start = Date.now();
+		let remainingAtStart = remaining;
+
+		function loop() {
+			if (playing) return;
+
+			requestAnimationFrame(loop);
+			remaining = remainingAtStart - (Date.now() - start);
+
+			if (remaining <= 0) {
+				//TODO the game has been lost
+				playing = false;
+			}
+		}
+
+		loop();
+	}
+
+	onMount(countdown);
 </script>
 
 <div class="game">
-    <header class="info">Header</header>
-    <section class="grid-container">
-        <Grid {grid} />
-    </section>
-    <footer class="info">Bottom</footer>
+	<header class="header">
+		<Countdown {remaining} duration={level.duration} />
+	</header>
+	<section class="grid-container">
+		<Grid
+			{grid}
+			on:found={(e) => {
+				emojisFound = [...emojisFound, e.detail.emoji];
+			}}
+			{emojisFound}
+		/>
+	</section>
+	<footer class="footer">
+		<EmojisFound {emojisFound} />
+	</footer>
 </div>
 
 <style>
-    .game {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-        font-size: min(1vmin, 0.3rem);
-    }
+	.game {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		font-size: min(1vmin, 0.3rem);
+	}
 
-    .info {
-        width: 80em;
-        height: 10em;
-        background-color: blueviolet;
-    }
+	.header,
+	.footer {
+		width: 80em;
+		height: 10em;
+	}
 
-    .grid-container {
-        width: 80em;
-        height: 80em;
-        background-color: cadetblue;
-    }
+	.grid-container {
+		width: 80em;
+		height: 80em;
+	}
 </style>
