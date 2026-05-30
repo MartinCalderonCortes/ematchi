523a34c0c994abe5731f5042e1ea502eade0ac35
Martin <mh.straus@hotmail.com>
2026-05-30 15:37:24 -0500

    Add logic to make visible or not the selected emoji according to the match selection on the board

--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,17 +1,33 @@
 <script lang="ts">
-    export let emoji: string
-
+	export let emoji: string;
+	export let selected: boolean;
+	export let isEmojiFound: boolean;
 </script>
 
-<div class="square">{emoji}</div>
+<div class="square" class:flipped={selected}>
+	<button on:click />
+
+	{#if !isEmojiFound}
+		<span>{emoji}</span>
+	{/if}
+</div>
 
 <style>
-    .square {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        background-color: darkgreen;
-        font-size: 2rem;
-    }
+	.square {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	button {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	span {
+		font-size: 7em;
+		pointer-events: none;
+	}
 </style>
