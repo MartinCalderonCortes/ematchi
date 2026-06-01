388bf2e8ad760d7b1b222074a981dd75ae848f82
Martin <mh.straus@hotmail.com>
2026-06-01 12:40:50 -0500

    Create Modal component

---
+++ src/routes/Modal.svelte
@@ -0,0 +1,19 @@
+<div class="modal-background">
+	<div class="modal">
+		<slot />
+	</div>
+</div>
+
+<style>
+	.modal-background {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		/* top: 0;
+		left: 0; */
+		display: grid;
+		place-items: center;
+		z-index: 10;
+		backdrop-filter: blur(10px);
+	}
+</style>
